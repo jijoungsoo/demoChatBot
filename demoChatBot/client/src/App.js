0e948546907e6593a33bcf610957da3eb85a72cf
jijs <jijsx@hotmail.com>
2022-02-01 22:25:49 +0900

    gogo

---
+++ demoChatBot/client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Typography } from 'antd';
+import {RobotOutlined} from '@ant-design/icons';
+import Chatbot from './Chatbot/Chatbot';
+const { Title } = Typography;
+
+function App() {
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+        <Title level={2} >CHAT BOT APP&nbsp;<RobotOutlined /></Title>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+       
+        <Chatbot />
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
